16f9f91444dee217d3b925f16311794e362aa2c6
PMatt <pmd.matthaei@gmail.com>
2016-07-29 23:46:37 +0200

    First postgres tests

---
+++ CSGOStatsED/CSGOStatsED/src/NPGSQLTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace CSGOStatsED.src
+{
+    class NPGSQLTest
+    {
+        public void test()
+        {
+            using (var conn = new NpgsqlConnection("Username=postgres;Password=arcoavid;Database=CSGODemos"))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = conn;
+
+                    // Insert some data
+                    cmd.CommandText = "INSERT INTO data (some_field) VALUES ('Hello world')";
+                    cmd.ExecuteNonQuery();
+
+                    // Retrieve all rows
+                    cmd.CommandText = "SELECT some_field FROM data";
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader.GetString(0));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
